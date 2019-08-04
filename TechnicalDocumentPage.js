0ae5682061b914d157aae94c01c18c57fe4f9849
guardiansfolly <48893214+guardiansfolly@users.noreply.github.com>
2019-08-04 11:31:51 -0500

    Javascript file

---
+++ TechnicalDocumentPage.js
@@ -0,0 +1,19 @@
+function dropdown1(){
+    document.getElementById('sub1').style.display="block";
+}
+
+function dropdown2(){
+    document.getElementById('sub2').style.display="block";
+}
+
+function dropdown3(){
+    document.getElementById('sub3').style.display="block";
+}
+
+function dropdown4(){
+    document.getElementById('sub4').style.display="block";
+}
+
+function dropdown5(){
+    document.getElementById('sub5').style.display="block";
+}
